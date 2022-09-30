fd4b5ad036016ea1bbec565defd98b7058d3aec2
dark <dark@kali.Dark>
2022-10-01 01:07:43 +0530

    Multiply the digits of the number entered by the user expect zero on python

---
+++ Python/multiplication.py
@@ -0,0 +1,5 @@
+number = input("enter an number: ")
+carpim=1
+for digitofnumber in str(numaber):
+  if int(digitofnumber) != 0:
+    multiplication*= int(digitofnumber)print("digits of the number: ",multiplication)
