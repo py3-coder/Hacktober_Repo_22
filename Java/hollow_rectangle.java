e96db8028b4fab4ee1e4d142bd47f567913f39bb
Anand kumar <77325274+Anand2350@users.noreply.github.com>
2022-10-03 20:32:30 +0530

    created hollow_rectangle.java
    Added the hollow rectangle code in java

---
+++ Java/hollow_rectangle.java
@@ -0,0 +1,20 @@
+package hollow_rectangle;
+
+public class Hollow_rectangle {
+
+	public static void main(String[] args) {
+		int n = 5;
+		int m = 4;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
