76abee0f1710e50df7939e400857c4e29c74741c
SHREYAS P VIJAY <vijayshreyasp@gmail.com>
2022-10-05 11:26:56 +0530

    Create First Come First Serve.c
    added fcfs program

---
+++ C/First Come First Serve.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+ 
+ int main()
+ 
+{
+    int n,bt[20],wt[20],tat[20],avwt=0,avtat=0,i,j;
+    printf("Enter total number of processes(maximum 20):");
+    scanf("%d",&n);
+ 
+    printf("nEnter Process Burst Timen");
+    for(i=0;i<n;i++)
+    {
+        printf("P[%d]:",i+1);
+        scanf("%d",&bt[i]);
+    }
+ 
+    wt[0]=0;   
+ 
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+    }
+ 
+    printf("nProcessttBurst TimetWaiting TimetTurnaround Time");
+ 
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i];
+        avwt+=wt[i];
+        avtat+=tat[i];
+        printf("nP[%d]tt%dtt%dtt%d",i+1,bt[i],wt[i],tat[i]);
+    }
+ 
+    avwt/=i;
+    avtat/=i;
+    printf("nnAverage Waiting Time:%d",avwt);
+    printf("nAverage Turnaround Time:%d",avtat);
+ 
+    return 0;
+}
