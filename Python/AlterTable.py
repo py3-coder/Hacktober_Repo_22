674fe20d31739aedcbcd0dc1af39905c275f8fa8
power-coder-java <114444122+power-coder-java@users.noreply.github.com>
2022-10-06 10:50:52 +0530

    Update AlterTable.py

--- Python/AlterTable.py
+++ Python/AlterTable.py
@@ -1,1 +1,31 @@
+import sqlite3
 
+# Connecting to sqlite
+connection_obj = sqlite3.connect('geek.db')
+
+# cursor object
+cursor_obj = connection_obj.cursor()
+
+# select from sqlite_master
+cursor_obj.execute("SELECT * FROM sqlite_master")
+
+table = cursor_obj.fetchall()
+print("Before changing the name of Table")
+print("The name of the table:", table[0][2])
+
+# Rename the SQLite Table
+renameTable = "ALTER TABLE GEEK RENAME TO GFG"
+cursor_obj.execute(renameTable)
+
+
+# select from sqlite_master
+cursor_obj.execute("SELECT * FROM sqlite_master")
+
+table = cursor_obj.fetchall()
+
+print("After changing the name of Table")
+print("The name of the table:", table[0][2])
+
+connection_obj.commit()
+
+connection_obj.close()
