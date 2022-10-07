226c48832a9502f8162b3294fceb7c0d0bad3e92
SNEHA C SHAJI <snehacshaji202@gmail.com>
2022-10-08 00:56:50 +0530

    Delete Python/personal_diary directory

--- Python/personal_diary/dear_diary/dear_diary/asgi.py
+++
@@ -1,4 +0,0 @@
-import os
-from django.core.asgi import get_asgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dear_diary.settings')
-application = get_asgi_application()
--- Python/personal_diary/dear_diary/entries/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Entry
-
-admin.site.register(Entry)
