94154ba18950187f51c8fe4e4de0ba82f347cd0b
SNEHA C SHAJI <snehacshaji202@gmail.com>
2022-10-07 23:59:22 +0530

    Add files via upload

---
+++ Python/personal_diary/dear_diary/dear_diary/asgi.py
@@ -0,0 +1,4 @@
+import os
+from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dear_diary.settings')
+application = get_asgi_application()
---
+++ Python/personal_diary/dear_diary/dear_diary/wsgi.py
@@ -0,0 +1,4 @@
+import os
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dear_diary.settings')
+application = get_wsgi_application()
