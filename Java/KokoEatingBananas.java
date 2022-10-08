467230f1fa8ae18f7073032c2bd66536b7c0dcb2
Ashish Kumar Saha <85429920+risingstar-bit@users.noreply.github.com>
2022-10-08 11:48:04 +0530

    Update KokoEatingBananas.java

--- Java/KokoEatingBananas.java
+++ Java/KokoEatingBananas.java
@@ -1,4 +1,6 @@
 /*
+PROBLEM STATEMENT:
+
 Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
 
 Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
