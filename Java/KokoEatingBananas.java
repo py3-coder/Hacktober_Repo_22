3c7c9e6cd3f2cf0d3a1ccd31cdd210c3d8b58e30
Ashish Kumar Saha <85429920+risingstar-bit@users.noreply.github.com>
2022-10-08 11:45:29 +0530

    Update KokoEatingBananas.java

--- Java/KokoEatingBananas.java
+++ Java/KokoEatingBananas.java
@@ -1,3 +1,28 @@
+/*
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+
+ 
+
+Example 1:
+
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+Example 2:
+
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+Example 3:
+
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
+*/
+
 package com.company.Searching;
 import java.util.ArrayList;
 import java.util.Arrays;
