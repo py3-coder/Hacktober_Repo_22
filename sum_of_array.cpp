aa79d2cb867c4d4a9b0e31a886142255cc02b143
Naitik Sharma <73165529+Naitik234@users.noreply.github.com>
2022-10-16 04:22:54 +0530

    Create sum_of_array.cpp

---
+++ sum_of_array.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// function to return sum of elements
+	// in an array of size n
+	int sum(int arr[], int n) {
+	    // code here
+	    int sum=0;
+	    for(int i=0;i<n;i++){
+	        sum=sum+arr[i];
+	    }
+	    return sum;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.sum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
