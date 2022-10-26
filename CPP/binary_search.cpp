f989c5907db69866122f894d5613f354b4ee0bce
Sanjay Gautam <sanjaykumargautam535@gmail.com>
2022-10-26 20:10:11 +0530

    fix conflict

--- CPP/binary_search.cpp
+++ CPP/binary_search.cpp
@@ -1,31 +1,54 @@
+//Importing the libraries
 #include <iostream>
 using namespace std;
 
-int binarySearch(int array[], int x, int low, int high) {
-  
-  while (low <= high) {
-    int mid = low + (high - low) / 2;
-
-    if (array[mid] == x)
-      return mid;
-
-    if (array[mid] < x)
-      low = mid + 1;
-
-    else
-      high = mid - 1;
-  }
-
-  return -1;
+//Function for Binary Search
+int BinarySearch (int arr[], int size, int element)
+{   int start=0, end=size-1;
+    int mid=(size+end)/2;
+    while(start<=end)
+    {
+        if(arr[mid] == element)
+        {
+           return mid;
+        }
+        else if(arr[mid]>element)
+        {
+           end=mid-1;
+        }
+        else
+        {
+           start=mid+1;
+        }
+        mid=(start+end)/2;
+    
+    }
+    return -1;
+    
 }
 
-int main(void) {
-  int array[] = {3, 4, 5, 6, 7, 8, 9};
-  int x = 4;
-  int n = sizeof(array) / sizeof(array[0]);
-  int result = binarySearch(array, x, 0, n - 1);
-  if (result == -1)
-    printf("Not found");
-  else
-    printf("Element is found at index %d", result);
+int main()
+{   int size,element;
+    cout<<"Enter the size of an array:";
+    cin>>size;
+    int arr[size];
+    cout<< "Enter the elements of array in increasing order:";
+    
+    for(int i=0;i<size;i++)
+    {
+       cin>>arr[i];
+    }
+    cout<< "Enter the element:";
+    cin>> element;
+    int found = BinarySearch (arr,size, element);
+    if(found == -1)
+    {
+       cout<< element <<" is not found in the array\n";
+    
+    }
+    else
+    {
+       cout << element <<" is found in the array at position "<< found+1 << endl;
+    }
+    return 0;
 }
